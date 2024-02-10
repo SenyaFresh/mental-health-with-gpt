b2002502e6a292bb04240a41ed57d495facf0871
Arseniy Smirnov <aasmirnov_16@edu.hse.ru>
2024-02-10 17:40:27 +0300

    Changed theme in ResultContainerComposable

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     api(project(":core:common"))
+    api(project(":core:themes"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- core/presentation/src/main/java/ru/edu/hse/presentation/ResultContainerComposable.kt
+++ core/presentation/src/main/java/ru/edu/hse/presentation/ResultContainerComposable.kt
@@ -3,9 +3,7 @@ package ru.edu.hse.presentation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +11,8 @@ import androidx.compose.ui.graphics.Color
 import ru.edu.hse.common.AuthenticationException
 import ru.edu.hse.common.Core
 import ru.edu.hse.common.ResultContainer
+import ru.edu.hse.mylibrary.DefaultButton
+import ru.edu.hse.mylibrary.DefaultText
 
 
 /**
@@ -36,21 +36,22 @@ fun ResultContainerComposable(
             is ResultContainer.Success -> {
                 onSuccess()
             }
+
             is ResultContainer.Error -> {
-                // TODO("Change Text and Button to app theme composables.")
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = Core.errorHandler.getUserFriendlyMessage(container.exception))
+                    DefaultText(text = Core.errorHandler.getUserFriendlyMessage(container.exception))
                     if (container.exception is AuthenticationException) {
-                        Button(onClick = { Core.appRestarter.restartApp() }) {
-                            Text(text = Core.resources.getString(R.string.core_presentation_logout))
-                        }
+                        DefaultButton(
+                            text = Core.resources.getString(R.string.core_presentation_logout),
+                            onClick = { Core.appRestarter.restartApp() })
                     } else {
-                        Button(onClick = { onTryAgain() }) {
-                            Text(text = Core.resources.getString(R.string.core_presentation_try_again))
-                        }
+                        DefaultButton(
+                            text = Core.resources.getString(R.string.core_presentation_try_again),
+                            onClick = { onTryAgain() })
                     }
                 }
             }
+
             is ResultContainer.Pending -> {
                 CircularProgressIndicator(color = Color.Black)
             }
