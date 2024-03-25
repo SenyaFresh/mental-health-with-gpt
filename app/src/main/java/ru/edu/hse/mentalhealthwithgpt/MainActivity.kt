246bf7fda57e4fcd98fb6e34cae9d47a91e51143
Arseniy Smirnov <aasmirnov_16@edu.hse.ru>
2024-03-25 15:11:27 +0300

    Fixed google-service issue and AndroidEntryPoint in MainActivity.

--- app/src/main/java/ru/edu/hse/mentalhealthwithgpt/MainActivity.kt
+++ app/src/main/java/ru/edu/hse/mentalhealthwithgpt/MainActivity.kt
@@ -3,11 +3,12 @@ package ru.edu.hse.mentalhealthwithgpt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import ru.edu.hse.mentalhealthwithgpt.navigation.BaseNavigation
 import ru.edu.hse.mentalhealthwithgpt.ui.theme.MentalHealthWithGPTTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
